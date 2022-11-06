d148da5c5f8c2845fc38fc6af2051d9d542b68e6
Mohamad J. Makki <malanbaki@transforat.com>
2022-11-06 11:00:21 +0300

    updating the entities

--- src/main/java/io/reactive/reactivespring/Domain/Delivery.java
+++ src/main/java/io/reactive/reactivespring/Domain/Delivery.java
@@ -4,7 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
@@ -12,4 +16,20 @@ import org.springframework.data.relational.core.mapping.Table;
 @Builder
 @Table("DELIVERY")
 public class Delivery {
+
+    @Id
+    @Column("DELIVERY_ID")
+    private UUID deliveryId;
+
+    @Column("ADDRESS_ID")
+    private UUID locationId;
+
+    @Column("DELIVERY_METHOD_ID")
+    private UUID deliveryMethodId;
+
+    @Column("DELIVERY_ASSIGNEE")
+    private String deliveryAssignee;
+
+    @Column("DELIVERY_BY")
+    private String deliveredBy;
 }
--- src/main/java/io/reactive/reactivespring/Domain/DeliveryMethod.java
+++ src/main/java/io/reactive/reactivespring/Domain/DeliveryMethod.java
@@ -4,7 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
@@ -12,4 +16,8 @@ import org.springframework.data.relational.core.mapping.Table;
 @Builder
 @Table("DELIVERY_METHOD")
 public class DeliveryMethod {
+
+    @Id
+    @Column("DELIVERY_METHOD_ID")
+    private UUID deliveryMethodId;
 }
