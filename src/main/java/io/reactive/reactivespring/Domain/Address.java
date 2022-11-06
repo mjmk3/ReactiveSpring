eef0afd7e6579efdd67f2b9677848247994f239a
Mohamad J. Makki <malanbaki@transforat.com>
2022-11-06 08:45:41 +0300

    updating the address entity

--- src/main/java/io/reactive/reactivespring/Domain/Address.java
+++ src/main/java/io/reactive/reactivespring/Domain/Address.java
@@ -4,7 +4,11 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
+
+import java.util.UUID;
 
 @Data
 @AllArgsConstructor
@@ -12,4 +16,32 @@ import org.springframework.data.relational.core.mapping.Table;
 @Builder
 @Table("ADDRESS")
 public class Address {
+
+    @Id
+    @Column("ADDRESS_ID")
+    private UUID uuid;
+
+    @Column("COUNTRY")
+    private String country;
+
+    @Column("PROVINCE")
+    private String province;
+
+    @Column("CITY")
+    private String city;
+
+    @Column("STREET_NUMBER")
+    private String streetNumber;
+
+    @Column("ADDRESS_LINE_ONE")
+    private String addressLineOne;
+
+    @Column("ADDRESS_LINE_TWO")
+    private String addressLineTwo;
+
+    @Column("POSTAL_CODE")
+    private String postalCode;
+
+    @Column("UNIT_NUMBER")
+    private String unitNumber;
 }
